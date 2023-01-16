fce4050bc675fc44de106ab8f6c74531f27b30fc
Oleg Izmerly <oizmerly@gmail.com>
2023-01-16 14:11:24 -0600

    new ui

--- Ristretto/RistrettoApp.swift
+++ Ristretto/RistrettoApp.swift
@@ -1,97 +1,89 @@
 import SwiftUI
-import IOKit.pwr_mgt
 
 @main
 struct TheApp: App {
-    @AppStorage("interval") private var interval:String = "1h"
-    private let intervals: Dictionary = [
-        "15 minutes": 15 * 60,
-        "30 minutes": 30 * 60,
-        "1 hour":     1 * 60 * 60,
-        "2 hours":    2 * 60 * 60,
-        "4 hours":    4 * 60 * 60,
-        "6 hours":    6 * 60 * 60,
-        "8 hours":    8 * 60 * 60,
-        "12 hours":   12 * 60 * 60
-    ]
-    enum Status { case stopped, running }
-    @State var status: Status = Status.stopped
+    @State private var status = AppStatus.stopped
     
-    // the timer
-    @State private var timer: Timer?
-    @State private var timeRemaining = 0
+    public func activate() {
+        status = .active
+        _ = disableScreenSleep()
+    }
+    
+    public func stop() {
+        status = .stopped
+        _ = enableScreenSleep()
+    }
     
     var body: some Scene {
-        MenuBarExtra("Ristretto", systemImage: status == Status.stopped ? "cup.and.saucer" : "cup.and.saucer.fill") {
-            VStack {
-                if status == Status.stopped {
-                    Text("☕ Ristretto (stopped)")
-                } else {
-                    Text("☕ Ristretto (\(secondsToString(s: timeRemaining)) left)")
-                }
-                
-                Divider()
-                
-                if status == Status.stopped {
-                    Picker(selection: $interval, label: Text("Enable for:")) {
-                        ForEach(intervals.sorted{ $0.value < $1.value }.map { $0.key }, id: \.self) {
-                            Text($0).tag($0)
-                        }
-                    }.pickerStyle(.menu)
-                }
-                
-                Button(action: {
-                    toggleSleep()
-                }, label: {
-                    if status == Status.stopped {
-                        Label("Start for \(interval)", systemImage: "play.fill")
-                            .labelStyle(.titleAndIcon)
-                            .frame(maxWidth: .infinity)
-                    } else {
-                        Label("Stop", systemImage: "stop.fill")
-                            .labelStyle(.titleAndIcon)
-                            .frame(maxWidth: .infinity)
-                    }
-                }).frame(maxWidth: .infinity)
-                
-                Divider()
-                
-                Button(action: {
-                    _ = enableScreenSleep()
-                    NSApplication.shared.terminate(nil)
-                }, label: {
-                    Label("Quit", systemImage: "xmark.circle.fill")
-                        .labelStyle(.titleAndIcon)
-                        .frame(maxWidth: .infinity)
-                }).frame(maxWidth: .infinity)
+        MenuBarExtra("App", systemImage: status == .active ? appIconActive : appIconStopped) {
+            AppMenu(app: self)
+        }.menuBarExtraStyle(.window).windowResizability(.contentMinSize)
+    }
+}
+
+struct AppMenu: View {
+    private let theApp: TheApp
+    @AppStorage("interval") private var interval:String = "1h"
+    @State private var status = AppStatus.stopped
+    @State private var timeRemaining = 0
+    @State private var timer: Timer?
+    private let timerStep = 60
     
-            }.padding(.top, 10)
-        }
+    init(app: TheApp) {
+        theApp = app
     }
     
-    func toggleSleep() {
-        if status == Status.running {
-            enableSleep()
+    private func toggle() {
+        if status == .active {
+            status = .stopped
+            theApp.stop()
+            timer?.invalidate()
         } else {
-            disableSleep()
-        }
-    }
-    
-    func disableSleep() {
-        status = Status.running
-        timeRemaining = intervals[interval]!
-        _ = disableScreenSleep()
-        timer = Timer.scheduledTimer(withTimeInterval: 60.0, repeats: true) { time in
-            timeRemaining -= 60
-            if timeRemaining <= 0 {
-                enableSleep()
+            status = .active
+            theApp.activate()
+            // start the timer
+            timeRemaining = predefinedIntervals[interval]!
+            timer = Timer.scheduledTimer(withTimeInterval: TimeInterval(timerStep), repeats: true) { _ in
+                timeRemaining -= timerStep
+                if timeRemaining <= 0 {
+                    toggle()
+                }
             }
         }
     }
     
-    func enableSleep() {
-        status = Status.stopped
-        _ = enableScreenSleep()
-        timer?.invalidate()
+    var body: some View {
+        VStack {
+            // the title
+            let titleStatus = (status == .active ? "(\(secondsToString(s: timeRemaining)) left)" : "(stopped)")
+            Text("☕ Ristretto " + titleStatus)
+
+            Divider()
+        
+            if status == .stopped {
+                Picker(selection: $interval, label: Text("Enable for:")) {
+                    ForEach(predefinedIntervals.sorted{ $0.value < $1.value }.map { $0.key }, id: \.self) {
+                        Text($0).tag($0)
+                    }
+                }.pickerStyle(.radioGroup)
+            }
+            Button(action: {
+                toggle()
+            }, label: {
+                if status == .stopped {
+                    Label("Start for \(interval)", systemImage: "play.fill").labelStyle(.titleAndIcon)
+                } else {
+                    Label("Stop", systemImage: "stop.fill").labelStyle(.titleAndIcon)
+                }
+            }).frame(maxWidth: .infinity)
+            
+            Button(action: {
+                theApp.stop()
+                NSApplication.shared.terminate(nil)
+            }, label: {
+                Label("Quit", systemImage: "xmark.circle.fill")
+                    .labelStyle(.titleAndIcon).frame(maxWidth: .infinity)
+            }).frame(maxWidth: .infinity)
+        }.padding(.top, 10)
     }
 }
---
+++ Ristretto/conf.swift
@@ -0,0 +1,19 @@
+enum AppStatus {
+    case stopped, active
+}
+
+let appIconStopped = "cup.and.saucer"
+let appIconActive = "cup.and.saucer.fill"
+
+// The intervals used in the app
+// TODO: i18n
+let predefinedIntervals = [
+    "15 minutes": 15 * 60,
+    "30 minutes": 30 * 60,
+    "1 hour":     1 * 60 * 60,
+    "2 hours":    2 * 60 * 60,
+    "4 hours":    4 * 60 * 60,
+    "6 hours":    6 * 60 * 60,
+    "8 hours":    8 * 60 * 60,
+    "12 hours":   12 * 60 * 60
+]
